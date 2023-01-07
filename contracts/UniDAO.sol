421b57d12d5ef20e3db18c9eb347107c92579dba
Vishnurr2k01 <vishnurr2k01@gmail.com>
2023-01-07 10:22:38 +0530

    contract ph1

---
+++ contracts/UniDAO.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract UniDAO is ReentrancyGuard,AccessControl {
+    bytes32 private immutable ADMIN_ROLE = keccak256("ADMIN");
+    bytes32 private immutable VOTER_ROLE = keccak256("VOTER");
+
+    address[] enrolled;
+    
+
+    uint32 public totalProposal;
+    uint32 immutable MIN_VOTE_DURATION = 2 minutes;
+
+    mapping(uint256 => ProposalStruct) private raisedProposals;
+
+    struct ProposalStruct{
+        uint256 proposalId;
+        string  title;
+        string  description;
+        uint256 reactions;
+        address proposer;
+        uint256 duration;
+        bool passed;
+        uint256 upvotes;
+        uint256 downvotes;
+    }
+
+    struct VotedStruct{
+        address voter;
+        uint256 timestamp;
+        bool choosen;
+        uint256 power;
+    }
+
+   event Action(      
+        address indexed initiator,
+       bytes32 role,
+       string message);
+
+   
+
+
+
+
+
+    modifier AdminOnly(){
+        require(hasRole(ADMIN_ROLE,msg.sender),"Unauthorized : only for admin");
+        _;
+    }
+
+    function enrollVoters(address[] addresses) private { 
+        for(int i=0;i<addresses.length;i++){
+            enrolled.push(addresses[i]);
+        }
+    emit Action(address(this),"ADMIN_ROLE","Addresses added successfully");
+    }
+
+    function createProposal( string memory _title,string memory _description,uint256 _duration ) private AdminOnly() returns(ProposalStruct memory){
+
+        _proposalId = totalProposal++;
+        ProposalStruct storage proposal = new raisedProposals[_proposalId];
+
+        proposal.proposalId = _proposalId;
+        proposal.proposer=msg.sender;
+        proposal.title=_title;
+        proposal.description=_description;
+        proposal.duration=_duration;
+        proposal.reactions=0;
+        proposal.downvotes=0;
+        proposal.upvotes=0;
+        proposal.passed=false;
+
+        emit Action(msg.sender,ADMIN_ROLE,"Proposal raised");
+    }
+}
