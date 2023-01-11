0e1831674b41fe47923eb7e24d663acdc98b1f72
Vishnurr2k01 <vishnurr2k01@gmail.com>
2023-01-11 09:03:41 +0530

    debug: proposal expired

--- contracts/UniDAO.sol
+++ contracts/UniDAO.sol
@@ -4,28 +4,25 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract UniDAO is ReentrancyGuard,AccessControl {
+contract UniDAO is ReentrancyGuard, AccessControl {
     bytes32 private immutable ADMIN_ROLE = keccak256("ADMIN");
     bytes32 private immutable VOTER_ROLE = keccak256("VOTER");
 
-    
-    
-
     uint256 public totalProposal;
     uint32 immutable MIN_VOTE_DURATION = 5 minutes;
+
     address[] adminAddresses;
 
     mapping(uint256 => ProposalStruct) private raisedProposals;
-    mapping(address=>uint256[]) private VoterVotes;
+    mapping(address => uint256[]) private VoterVotes;
     mapping(uint256 => VotedStruct[]) private VotedOn;
-
-    mapping(address=>VoterStruct) private enrolled;
+    mapping(address => VoterStruct) private enrolled;
 
     //structs
     struct ProposalStruct {
         uint256 proposalId;
-        string  title;
-        string  description;
+        string title;
+        string description;
         uint256 reactions;
         address proposer;
         uint256 duration;
@@ -34,129 +31,127 @@ contract UniDAO is ReentrancyGuard,AccessControl {
         uint256 downvotes;
     }
 
-    struct VotedStruct{
+    struct VotedStruct {
         address voter;
         uint256 timestamp;
         uint256 choosen;
         uint256 power;
     }
 
-    struct VoterStruct{
+    struct VoterStruct {
         uint256 power;
-        
     }
 
     //events
-   event Action(      
-        address indexed initiator,
-       bytes32 role,
-       string message);
+    event Action(address indexed initiator, bytes32 role, string message);
 
-   
+    constructor(address[] memory addr) public {
+        for (uint256 i = 0; i < addr.length; i++) {
+            adminAddresses.push(addr[i]);
+        }
+    }
 
-   constructor(address[] addr) public{
-       for(uint256 i=0;i<addr.length;i++){
-           adminAddresses.push(addr);
-       }
-   }
-   //modifiers
+    //modifiers
 
-    modifier AdminOnly(){
-        require(hasRole(ADMIN_ROLE,msg.sender),"Unauthorized : only for admin");
+    modifier AdminOnly() {
+        require(
+            hasRole(ADMIN_ROLE, msg.sender),
+            "Unauthorized : only for admin"
+        );
         _;
     }
 
-    modifier VoterOnly(){
-  require(hasRole(VOTER_ROLE,msg.sender),"Unauthorized : only for people enrolled in this institution");
-        _;   
+    modifier VoterOnly() {
+        require(
+            hasRole(VOTER_ROLE, msg.sender),
+            "Unauthorized : only for people enrolled in this institution"
+        );
+        _;
     }
 
-    //admin actions 
+    //admin actions
 
+    //                      enroll new voters
 
-
-//                      enroll new voters
-
-    function enrollVoters(address[] memory addresses,uint256[] memory _power) public { 
-        for(uint256 i=0;i<addresses.length;i++){
-            if(enrolled[addresses[i]].power>0){
+    function enrollVoters(address[] memory addresses, uint256[] memory _power)
+        public
+    {
+        for (uint256 i = 0; i < addresses.length; i++) {
+            if (enrolled[addresses[i]].power > 0) {
                 continue;
             }
             VoterStruct storage voter = enrolled[addresses[i]];
             voter.power = _power[i];
-            _setupRole(VOTER_ROLE,addresses[i]);
+            _setupRole(VOTER_ROLE, addresses[i]);
         }
 
-    emit Action(address(this),"ADMIN_ROLE","Addresses added successfully");
+        emit Action(
+            address(this),
+            "ADMIN_ROLE",
+            "Addresses added successfully"
+        );
     }
 
+    //                     SET ADMIN ROLE
 
-
-//                     SET ADMIN ROLE
-
-
-
-//set roles for addressed passed through constructor
-  function setAdminRole()public{
-        for(uint256 i=0;i<adminAddresses.length;i++){
-            _setupRole(ADMIN_ROLE,adminAddresses[i]);
-        _setupRole(VOTER_ROLE,adminAddresses[i]);
-       }
+    //set roles for addressed passed through constructor
+    function setAdminRole() public {
+        for (uint256 i = 0; i < adminAddresses.length; i++) {
+            _setupRole(ADMIN_ROLE, adminAddresses[i]);
+            _setupRole(VOTER_ROLE, adminAddresses[i]);
+        }
     }
 
-//add new admin
-    function setnewAdmin(address addr)public{
-        _setupRole(ADMIN_ROLE,addr);
-        _setupRole(VOTER_ROLE,addr);
+    //add new admin
+    function setnewAdmin(address addr) public {
+        _setupRole(ADMIN_ROLE, addr);
+        _setupRole(VOTER_ROLE, addr);
     }
 
     //proposal and related functions
 
-    
-    function createProposal( string memory _title,string memory _description
-    // ,uint256 _duration
-     ) external AdminOnly() returns(ProposalStruct memory){
-
-       uint256 _proposalId = totalProposal++;
-         ProposalStruct storage proposal = raisedProposals[_proposalId];
+    function createProposal(
+        string memory _title,
+        string memory _description,
+        uint256 _duration
+    ) external AdminOnly returns (ProposalStruct memory) {
+        uint256 _proposalId = totalProposal++;
+        ProposalStruct storage proposal = raisedProposals[_proposalId];
 
         proposal.proposalId = _proposalId;
-        proposal.proposer=msg.sender;
-        proposal.title=_title;
-        proposal.description=_description;
-        proposal.duration=MIN_VOTE_DURATION;
-        proposal.reactions=0;
-        proposal.downvotes=0;
-        proposal.upvotes=0;
-        // proposal.passed=false;
+        proposal.proposer = msg.sender;
+        proposal.title = _title;
+        proposal.description = _description;
+        proposal.duration = block.timestamp + _duration;
+        proposal.reactions = 0;
+        proposal.downvotes = 0;
+        proposal.upvotes = 0;
 
-        emit Action(msg.sender,ADMIN_ROLE,"Proposal raised");
+        emit Action(msg.sender, ADMIN_ROLE, "Proposal raised");
     }
 
-    function performVote(uint256 _proposalId,uint256 choosen) public VoterOnly(){
+    function performVote(uint256 _proposalId, uint256 choosen)
+        public
+        VoterOnly
+    {
         ProposalStruct storage proposal = raisedProposals[_proposalId];
         handleVoting(proposal);
         uint256 _power = enrolled[msg.sender].power;
-        if(choosen==1)proposal.upvotes+=_power;
-        else proposal.downvotes+=_power;
+        if (choosen == 1) proposal.upvotes += _power;
+        else proposal.downvotes += _power;
 
         VoterVotes[msg.sender].push(_proposalId);
 
         VotedOn[proposal.proposalId].push(
-       VotedStruct(payable(msg.sender), block.timestamp, choosen,_power)
+            VotedStruct(payable(msg.sender), block.timestamp, choosen, _power)
         );
 
-
-        emit Action(
-            msg.sender,
-            VOTER_ROLE,
-            "voted"
-        );
+        emit Action(msg.sender, VOTER_ROLE, "voted");
     }
 
-    function handleVoting(ProposalStruct storage proposal)private{
-        if(proposal.passed || proposal.duration<=block.timestamp){
-            proposal.passed=true;
+    function handleVoting(ProposalStruct storage proposal) private {
+        if (proposal.passed || proposal.duration <= block.timestamp) {
+            proposal.passed = true;
             revert("Proposal has expired");
         }
 
@@ -168,13 +163,27 @@ contract UniDAO is ReentrancyGuard,AccessControl {
         }
     }
 
-    function getProposals()public view returns(ProposalStruct[] memory props){
-
+    function getProposals()
+        public
+        view
+        returns (ProposalStruct[] memory props)
+    {
         props = new ProposalStruct[](totalProposal);
 
-        for(uint256 i=0;i<totalProposal;i++){
-            props[i]=raisedProposals[i];
+        for (uint256 i = 0; i < totalProposal; i++) {
+            props[i] = raisedProposals[i];
         }
     }
-    
+
+    function getProposal(uint256 _proposalId)
+        public
+        view
+        returns (ProposalStruct memory)
+    {
+        return raisedProposals[_proposalId];
+    }
+
+    function isVoter() public view returns (bool) {
+        return enrolled[msg.sender].power > 0;
+    }
 }
