06e39efc92273d32cadb54dd7ebc2c7a3249a22f
Rakhi R Panicker <rakhiramadas15@gmail.com>
2023-02-07 07:28:23 +0530

    Home Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,30 @@
-
-import Adminop from "./pages/Adminop"
-import Navbar from "./components/Navbar"
+//import Adminop from "./pages/Adminop"
+//import Navbar from "./components/Navbar"
+//import React from "react";
+//import Footer from "./components/Footer";
+//import Header from "./components/Header";
+//import Hero from "./components/Hero";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Frontpg from "./pages/Frontpg";
 function App() {
   return (
-  <Router>
-    <Navbar />
-    <Routes>
-    <Route path='/Adminop' element={<Adminop/>} />
-    </Routes>
-   </Router>
+    <div>
+      {
+        <Router>
+          <Routes>
+            <Route path='/' element={<Frontpg/>} />
+          </Routes>
+        </Router>
+      }
+    </div>
+    
+ // <Router>
+   // <Navbar />
+    //<Routes>
+    //<Route path='/Adminop' element={<Adminop/>} />
+    //</Routes>
+   //</Router>
 
-)
+);
 }
-
 export default App;
---
+++ frontend/src/pages/Frontpg.js
@@ -0,0 +1,17 @@
+//import React,{useState,useEffect} from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Hero from '../components/Hero'
+//import {BrowserRouter as Router,Routes,Route} from "react-router-dom" ;
+const Frontpg = () => {
+  return (
+    <>
+        <Header />
+        <Hero />
+        <Footer />
+    </>
+
+  )
+}
+
+export default Frontpg
