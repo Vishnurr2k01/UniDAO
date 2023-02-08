5898146668dbbe6a7bc4daa110b3a3526022bcd0
GowriJ-15 <gowrijayakumar@gmail.com>
2023-02-08 18:39:38 +0530

    appmodified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
-
-import Adminop from "./pages/Adminop"
-import Navbar from "./components/Navbar"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Adminop from "./pages/Adminop"
+
+
 function App() {
   return (
+    <div>
+      {
   <Router>
-    <Navbar />
     <Routes>
-    <Route path='/Adminop' element={<Adminop/>} />
+    
+    <Route path='/Adminop' element={<Adminop/>}/>
     </Routes>
    </Router>
-
+}
+</div>
 )
 }
 
