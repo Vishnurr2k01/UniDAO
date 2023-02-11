ef2eca8ee934f6223e0be10c963ed87f25b7d77d
GowriJ-15 <gowrijayakumar@gmail.com>
2023-02-11 15:27:42 +0530

    modal1

---
+++ frontend/src/components/modala.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+const Modal = () => {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <>
+      
+      {showModal ? (
+        <>
+          <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div className="flex items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
+                  <h1 className="text-xl font=semibold">Add Proposal</h1>
+                  <button
+                    className="bg-transparent border-0 text-black float-right"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="text-black opacity-7 h-6 w-6 text-xl block bg-teal-400 py-0 rounded-full">
+                      x
+                    </span>
+                  </button>
+                </div>
+                <div className="relative p-6 flex-auto">
+                  <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full">
+                    <label className="block text-black text-sm font-bold mb-1">
+                      Title
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
+                    <label className="block text-black text-sm font-bold mb-1">
+                      Description
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
+                    <label className="block text-black text-sm font-bold mb-1">
+                      Duration
+                    </label>
+                    <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black" />
+                    
+                    
+                  </form>
+                </div>
+                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
+                  <button
+                    className="text-black background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                  <button
+                    className="text-black bg-teal-400 active:bg-teal-400 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export default Modal;
