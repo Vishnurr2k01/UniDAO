6c6269065146c9fa5fe72caff4ae786f9629124b
GowriJ-15 <gowrijayakumar@gmail.com>
2023-02-20 19:58:26 +0530

    modifiedadminhero

--- frontend/src/components/adminhero.js
+++ frontend/src/components/adminhero.js
@@ -23,14 +23,26 @@ const HeroA = () => {
       <nav className='fixed'>
     <Navbar />
     </nav>
-      <div className='w-screen  pl-60  text-[#00df9a] '>
+      <div className='w-screen  pl-40  text-[#00df9a] '>
        
-        <h1 className='py-20 pl-60 text-xl font-extrabold'>
+        <h1 className='py-20 pl-60 text-xxl font-extrabold'>
           {/*<span className='blue'>Decentralized Decision</span> Making*/}
-        </h1>
-        <p className='text-2xl py-80 pl-80 italic animate-pulse delay-500'>
           
-           "The power to add proposals and add decision makers vests in you!""
+          <h1 className='md:text-6xl sm:text-6xl text-4xl font-bold md:py-0 pl-40 text-white'>
+          UNIDAO
+        </h1>
+        
+        </h1>
+        {/*<div className='flex justify-center items-center'>*/}
+        <h1 className='py-0  pl-60'>
+          <p className='md:text-4xl sm:text-6xl text-6l font-semibold md:py-0'>
+            DECENTRALIZATION AND INCLUSION
+          </p>
+          </h1>
+          
+        <p className='text-3xl py-11 pl-40 italic animate-pulse delay-500 absolute right text-white'>
+          
+           "The power to add proposals and add decision makers vests in you!"
         </p>
 
       </div>
