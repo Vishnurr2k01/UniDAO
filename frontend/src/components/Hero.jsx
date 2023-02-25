add1c6eebe0655107a8bfc010567c0890fabd5b9
Vishnurr2k01 <vishnurr2k01@gmail.com>
2023-02-25 21:43:35 +0530

    bug test

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,7 +2,9 @@ import React, { useEffect,useRef } from 'react';
 import Typed from 'typed.js'
 import {connectWallet, createProposal, isAdmin} from '../services/Blockchain.services';
 import { getGlobalState, useGlobalState } from '../store';
+import { useNavigate } from 'react-router-dom';
 const Hero = () => {
+  const navigate = useNavigate()
 const el = useRef(null)
   const [connectedAccount] = useGlobalState('connectedAccount')
   useEffect(() => {
@@ -35,7 +37,7 @@ const el = useRef(null)
         <p className='text-[#00df9a] font-bold p-2' >
           GROWING WITH DAO
         </p>
-        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>
+        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6' onClick={()=>navigate('/admin')}>
           Decentralised Decision Making Platform
         </h1>
         <div className='flex justify-center items-center'>
