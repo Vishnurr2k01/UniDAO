dd77e3989258bfaa872870c8d77afc22933cdfe8
Vishnurr2k01 <vishnurr2k01@gmail.com>
2023-03-08 20:32:15 +0530

    .

--- frontend/src/services/Blockchain.services.js
+++ frontend/src/services/Blockchain.services.js
@@ -53,13 +53,13 @@ const getEthereumContract = async () => {
     const networkId = await window.web3.eth.net.getId();
     const networlData = abi.networks[networkId];
     if (networlData) {
-//       const contract = new window.web3.eth.Contract(
-//         abi.abi,
-// networlData.address      );
       const contract = new window.web3.eth.Contract(
-        abidata,
-        "0x349ff7276a22d79f0b6384265fdad007da539e2a"
-      );
+        abi.abi,
+networlData.address      );
+      // const contract = new window.web3.eth.Contract(
+      //   abidata,
+      //   "0x349ff7276a22d79f0b6384265fdad007da539e2a"
+      // );
       setGlobalState("contract", contract);
       return contract;
     } else return null;
